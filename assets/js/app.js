9a15187e2ca5d7b1cee31c56a5cdbc65fd6b3b8d
Kiplangat Edwin <kiplangatedwin734@gmail.com>
2023-07-31 17:05:25 +0300

    Indentation of code

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,19 +3,18 @@
 const carousel = [...document.querySelectorAll('.carousel img')];
 let carouselImageIndex = 0;
 
-const changeCarousel = () =>{
-          carousel[carouselImageIndex].classList.toggle('active');
-          if(carouselImageIndex >=carousel.length-1){
-                    carouselImageIndex=0;
-          }
-          else{
-                    carouselImageIndex++;
-          }
-          carousel[carouselImageIndex].classList.toggle('active');
+const changeCarousel = () => {
+    carousel[carouselImageIndex].classList.toggle('active');
+    if (carouselImageIndex >= carousel.length - 1) {
+        carouselImageIndex = 0;
+    } else {
+        carouselImageIndex++;
+    }
+    carousel[carouselImageIndex].classList.toggle('active');
 }
-setInterval(() =>{
-          changeCarousel();
-},3000)
+setInterval(() => {
+    changeCarousel();
+}, 3000)
 
 
 ///Navigations///
@@ -25,17 +24,17 @@ setInterval(() =>{
 const musicPlayer = document.querySelector('.music-player');
 
 let clickCount = 1;
-musicPlayer.addEventListener('click' , ()=>{
-          if(clickCount >=2){
-                    musicPlayer.classList.add('active');
-                    clickCount =1;
-                    return;
-          }
-          clickCount++;
-          setTimeout(() =>{
-                    clickCount =1;
+musicPlayer.addEventListener('click', () => {
+    if (clickCount >= 2) {
+        musicPlayer.classList.add('active');
+        clickCount = 1;
+        return;
+    }
+    clickCount++;
+    setTimeout(() => {
+        clickCount = 1;
 
-          }, 250);
+    }, 250);
 });
 
 
@@ -43,8 +42,8 @@ musicPlayer.addEventListener('click' , ()=>{
 
 const backToHome = document.querySelector('.music-player .back-btn');
 
-backToHome.addEventListener('click' , () =>{
-          musicPlayer.classList.remove('active');
+backToHome.addEventListener('click', () => {
+    musicPlayer.classList.remove('active');
 })
 
 
@@ -53,15 +52,15 @@ backToHome.addEventListener('click' , () =>{
 const playlist = document.querySelector('.playlist');
 const navBtn = document.querySelector('.music-player .nav-btn');
 
-navBtn.addEventListener ('click' , () =>{
-          playlist.classList.add('active')
+navBtn.addEventListener('click', () => {
+    playlist.classList.add('active')
 })
 
 ///Back From PlayList///
 
 const backToPlayer = document.querySelector('.playlist .back-btn');
-backToPlayer.addEventListener('click' , ()=> {
-          playlist.classList.remove('active')
+backToPlayer.addEventListener('click', () => {
+    playlist.classList.remove('active')
 })
 
 ///Music///
@@ -71,14 +70,14 @@ let currentMusic = 0;
 const music = document.querySelector('#audio-source');
 
 const seekBar = document.querySelector('.music-seek-bar');
-const songName =document.querySelector('.current-song-name');
+const songName = document.querySelector('.current-song-name');
 const artistName = document.querySelector('.artist');
 const coverImage = document.querySelector('.cover');
 const currentMusicTime = document.querySelector('.current-time');
 const duration = document.querySelector('.duration');
-const queue =document.querySelectorAll('.queue')
-const pageTitle =document.querySelector('.htittle')
-const pageIcon =document.querySelector('.aicon')
+const queue = document.querySelectorAll('.queue')
+const pageTitle = document.querySelector('.htittle')
+const pageIcon = document.querySelector('.aicon')
 
 //all buttons//
 
@@ -91,16 +90,16 @@ const repeatBtn = document.querySelector('.uil-redo');
 const volumeBtn = document.querySelector('.uil-volume');
 const volumeSlider = document.querySelector('.volume-slider');
 const volumePercentage = document.querySelector('.vperc');
- const opNav= document.querySelector('.opnav');
+const opNav = document.querySelector('.opnav');
 
 //Play Button Events///
-playBtn.addEventListener('click' , ()=>{
+playBtn.addEventListener('click', () => {
     music.play()
-          playBtn.classList.remove('active');
-          pauseBtn.classList.add('active');
+    playBtn.classList.remove('active');
+    pauseBtn.classList.add('active');
 });
 
-window.addEventListener('load',()=>{
+window.addEventListener('load', () => {
     music.pause();
     playBtn.classList.remove('active');
     pauseBtn.classList.add('active');
@@ -109,19 +108,19 @@ window.addEventListener('load',()=>{
 
 ///Pause Button Events///
 
-pauseBtn.addEventListener('click' , ()=>{
+pauseBtn.addEventListener('click', () => {
     music.pause();
-          pauseBtn.classList.remove('active');
-          playBtn.classList.add('active');
+    pauseBtn.classList.remove('active');
+    playBtn.classList.add('active');
 });
 
-opNav.addEventListener('click',()=>{
+opNav.addEventListener('click', () => {
     musicPlayer.classList.add('active');
 })
 
 
 //Function For Music///
-const setMusic  =(i)=> {
+const setMusic = (i) => {
     seekBar.value = 0;
     let song = songs [i];
     currentMusic = i;
@@ -131,111 +130,109 @@ const setMusic  =(i)=> {
     songName.innerHTML = song.name;
     artistName.innerHTML = song.artist;
     coverImage.src = song.cover;
-    pageTitle.innerHTML = ' Music Player -' + song.name ;
+    pageTitle.innerHTML = ' Music Player -' + song.name;
     pageIcon.src = song.cover;
 
     setTimeout(() => {
         seekBar.max = music.duration;
         duration.innerHTML = formatTime(music.duration);
     }, 300);
-    currentMusicTime.innerHTML ="00:00"
+    currentMusicTime.innerHTML = "00:00"
     queue.forEach(item => item.classList.remove('active'))
 
     queue[currentMusic].classList.add('active')
 }
- //format duration//
+//format duration//
 
-const formatTime =(time) =>{
-    let min = Math.floor(time/60);
-    if (min < 10) { 
+const formatTime = (time) => {
+    let min = Math.floor(time / 60);
+    if (min < 10) {
         min = '0' + min;
     }
-    let sec = Math.floor(time%60);
-    if (sec <10){
-        sec = '0' +sec;
+    let sec = Math.floor(time % 60);
+    if (sec < 10) {
+        sec = '0' + sec;
     }
-    return  `${min} : ${sec}`;
+    return `${min} : ${sec}`;
 }
 
 /// seekBar events
-setInterval(() =>{
+setInterval(() => {
     seekBar.value = music.currentTime;
-    currentMusicTime.innerHTML = formatTime( music.currentTime);
-    if(Math.floor(music.currentTime) == Math.floor(seekBar.max)){
-        if(repeatBtn.className.includes('active')){
+    currentMusicTime.innerHTML = formatTime(music.currentTime);
+    if (Math.floor(music.currentTime) == Math.floor(seekBar.max)) {
+        if (repeatBtn.className.includes('active')) {
             setMusic(currentMusic)
             playBtn.click()
-        }else{
+        } else {
             fowardBtn.click();
         }
     }
-},500);
+}, 500);
 
-seekBar.addEventListener('change',() =>{
-    music.currentTime =seekBar.value;
+seekBar.addEventListener('change', () => {
+    music.currentTime = seekBar.value;
 })
 
 //Foward btn
 
-fowardBtn.addEventListener('click',() =>{
-    if (currentMusic >= songs.length - 1){
-        currentMusic =0;
-    }
-    else {
+fowardBtn.addEventListener('click', () => {
+    if (currentMusic >= songs.length - 1) {
+        currentMusic = 0;
+    } else {
         currentMusic++;
     }
     setMusic(currentMusic);
     playBtn.click();
 
-    if(clickCount >=2){
+    if (clickCount >= 2) {
         musicPlayer.classList.remove('active');
-        clickCount =1;
+        clickCount = 1;
         return;
-}
+    }
 })
 
 ///Backward btn
 
-backBtn.addEventListener('click' , ()=>{
-    if (currentMusic <= 0){
-        currentMusic= songs.length - 1;
-    }
-    else {
+backBtn.addEventListener('click', () => {
+    if (currentMusic <= 0) {
+        currentMusic = songs.length - 1;
+    } else {
         currentMusic--;
     }
     setMusic(currentMusic);
     playBtn.click();
 
-    if(clickCount >=2){
+    if (clickCount >= 2) {
         musicPlayer.classList.remove('active');
-        clickCount =1;
+        clickCount = 1;
         return;
-}
-    
+    }
+
 })
 
 
-repeatBtn.addEventListener('click', ()=>{
+repeatBtn.addEventListener('click', () => {
     repeatBtn.classList.toggle('active');
 })
 
 
-volumeBtn.addEventListener('click',()=>{
+volumeBtn.addEventListener('click', () => {
     volumeBtn.classList.toggle('active')
     volumeSlider.classList.toggle('active')
 
 })
-volumeSlider.addEventListener('input',() =>{
+volumeSlider.addEventListener('input', () => {
     music.volume = volumeSlider.value;
-    if(volumeSlider.value >=1){
-        volumePercentage.innerHTML='100'
-    }else{
-        volumePercentage.innerHTML= volumeSlider.value*100
+    if (volumeSlider.value >= 1) {
+        volumePercentage.innerHTML = '100'
+    } else {
+        volumePercentage.innerHTML = volumeSlider.value * 100
     }
 })
 
-queue.forEach((item,i) =>{
-    item.addEventListener('click' ,()=>{
+queue.forEach((item, i) => {
+    item.addEventListener('click', () => {
         setMusic(i)
         playBtn.click();
     })
--- assets/js/data.js
+++ assets/js/data.js
@@ -1,50 +1,50 @@
-let songs =[
-          {
-          name :'Midnight',
-          path :'assets/audio/Alesso & Liam Payne - Midnight.m4a',
-          artist :'Alesso & Liam Payne',
-          cover :'assets/images/3.jpg'
-},
-{
-          name:' Wave of You',
-          path : 'assets/audio/Surfaces - Wave of You.m4a',
-          artist :'Surfaces',
-          cover :'assets/images/11.jpg'
-} ,
-{
-          name:'Closer',
-          path : 'assets/audio/The Chainsmokers - Closer ft. Halsey.m4a',
-          artist :'The Chainsmokers (ft. Halsey)',
-          cover :'assets/images/7.jpg'
-} ,
-{
-          name:'Collide',
-          path : 'assets/audio/Tiana Major9, EARTHGANG - Collide (From -Queen & Slim- The Soundtrack-) .m4a',
-          artist :'Tianor Major9 ,EARTHGANG',
-          cover :'assets/images/5.jpg'
-},
-{
-          name:'With You In The Morning',
-          path : 'assets/audio/With You In The Morning - Carl Storm.m4a',
-          artist :'Calm Storm',
-          cover :'assets/images/12.jpg'
-},
-{
-          name:'Into Your Arms',
-          path : 'assets/audio/Witt Lowry - Into Your Arms (feat. Ava Max)  .m4a',
-          artist :'Witt Lowry (feat. Ava Max)',
-          cover :'assets/images/2.jpg'
-},
-{
-          name:'Like I Do',
-          path : 'assets/audio/Witt Lowry - Like I Do.m4a',
-          artist :'Witt Lowry',
-          cover :'assets/images/8.jpg'
-},
-{
-          name:'Congratulations ft Buchi',
-          path : 'assets/audio/Ada Ehi - Congratulations ft Buchi.m4a',
-          artist :'Ada Ehi',
-          cover :'assets/images/6.jpg'
-}
+let songs = [
+    {
+        name: 'Midnight',
+        path: 'assets/audio/Alesso & Liam Payne - Midnight.m4a',
+        artist: 'Alesso & Liam Payne',
+        cover: 'assets/images/3.jpg'
+    },
+    {
+        name: ' Wave of You',
+        path: 'assets/audio/Surfaces - Wave of You.m4a',
+        artist: 'Surfaces',
+        cover: 'assets/images/11.jpg'
+    },
+    {
+        name: 'Closer',
+        path: 'assets/audio/The Chainsmokers - Closer ft. Halsey.m4a',
+        artist: 'The Chainsmokers (ft. Halsey)',
+        cover: 'assets/images/7.jpg'
+    },
+    {
+        name: 'Collide',
+        path: 'assets/audio/Tiana Major9, EARTHGANG - Collide (From -Queen & Slim- The Soundtrack-) .m4a',
+        artist: 'Tianor Major9 ,EARTHGANG',
+        cover: 'assets/images/5.jpg'
+    },
+    {
+        name: 'With You In The Morning',
+        path: 'assets/audio/With You In The Morning - Carl Storm.m4a',
+        artist: 'Calm Storm',
+        cover: 'assets/images/12.jpg'
+    },
+    {
+        name: 'Into Your Arms',
+        path: 'assets/audio/Witt Lowry - Into Your Arms (feat. Ava Max)  .m4a',
+        artist: 'Witt Lowry (feat. Ava Max)',
+        cover: 'assets/images/2.jpg'
+    },
+    {
+        name: 'Like I Do',
+        path: 'assets/audio/Witt Lowry - Like I Do.m4a',
+        artist: 'Witt Lowry',
+        cover: 'assets/images/8.jpg'
+    },
+    {
+        name: 'Congratulations ft Buchi',
+        path: 'assets/audio/Ada Ehi - Congratulations ft Buchi.m4a',
+        artist: 'Ada Ehi',
+        cover: 'assets/images/6.jpg'
+    }
 ]
